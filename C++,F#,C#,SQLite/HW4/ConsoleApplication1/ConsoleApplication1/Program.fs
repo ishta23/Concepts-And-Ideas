510bb33bcf48f6dbfdf27340ee24fa037ad8bef0
ishta23 <bhagatishta@gmail.com>
2016-12-17 20:06:10 -0600

    341 up

---
+++ C++,F#,C#,SQLite/HW4/ConsoleApplication1/ConsoleApplication1/Program.fs
@@ -0,0 +1,17 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+
+let a x y z =
+  423
+
+let F x y =
+  x *y
+
+[<EntryPoint>]
+let main argv = 
+    let a = F 10
+    let L =[]
+    let b = List.length L
+    printf"%A" a
+    0 // return an integer exit code
---
+++ C++,F#,C#,SQLite/HW5/hw5-functions/Program.fs
@@ -0,0 +1,16 @@
+﻿#light
+
+open Functions
+
+//
+// main() program:  when you run as an .exe
+//
+[<EntryPoint>]
+let main argv =
+  printfn ""
+  printfn ""
+  printfn "** Run unit tests via Test>>Windows>>Test Explorer..."
+  printfn ""
+  printfn ""
+  0 // return success
+
